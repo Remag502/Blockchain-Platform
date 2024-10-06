aadd3d09a900c36fd7d034068375b7ecad431f86
Remag501-Desktop <evanverma2003@gmail.com>
2024-10-06 02:52:15 -0700

    Setup paths for django backend, and an http json response for get requests

--- blockchain/views.py
+++ blockchain/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
+def say_hello(request):
+    # Check if the request method is GET
+    if request.method == 'GET':
+        message = {
+            "message": "Hello World"
+        }
+        # Return the JSON response
+        return JsonResponse(message)
+    else:
+        return JsonResponse({"error": "Invalid request method."}, status=400)
+    # return HttpResponse("Hello")
