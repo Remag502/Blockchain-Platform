06930305375b742a05f2a043010f03e8cda51fdf
Remag501-Desktop <evanverma2003@gmail.com>
2024-10-06 02:20:55 -0700

    Added bootstrap-react and a sample component to page to test.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Add bootstrap to project
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
